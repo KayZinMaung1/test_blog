8ad1bdc7aaef753a2bfeab4c547879559289de81
KayZinMaung1 <kayzinmg363@gmail.com>
2022-09-24 22:01:26 +0630

    first laravel deployment heroku

---
+++ app/Http/Controllers/Article/ArticleController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Article;
+
+use App\Http\Controllers\Controller;
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        return Article::all();
+    }
+
+    public function detail($id)
+    {
+        return "Controller - Article Detail - $id";
+    }
+}
